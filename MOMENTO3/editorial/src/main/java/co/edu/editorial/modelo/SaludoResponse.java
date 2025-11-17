aaa03899eadc874c50296c095c6a304589d8e4e4
AndresMira21 <andres.mirandae@campusucc.edu.co>
2025-11-16 21:46:20 -0500

    subiendo actividades momento 3

---
+++ MOMENTO3/editorial/src/main/java/co/edu/editorial/modelo/SaludoResponse.java
@@ -0,0 +1,19 @@
+package co.edu.editorial.modelo;
+
+public class SaludoResponse {
+
+    private String saludo;
+
+    public SaludoResponse(String saludo) {
+        super();
+        this.saludo = saludo;
+    }
+
+    public String getSaludo() {
+        return saludo;
+    }
+
+    public void setSaludo(String saludo) {
+        this.saludo = saludo;
+    }
+}
